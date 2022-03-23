a2b36d3a63a407502e71640d8d505104e60212a8
expresoviter <89355159+expresoviter@users.noreply.github.com>
2022-03-23 12:56:47 +0200

    Delete OP/Lab_2 directory

--- OP/Lab_2/Lab2_py/Lab2_py.py
+++
@@ -1,6 +0,0 @@
-from binFiles import *
-
-act=int(input("Очистити дані файлу (0) чи додати (1) до існуючих? : "))
-inputData(act)
-n=int(input("Уведіть кількість клієнтів: "))
-verify(n, "input.bin")
